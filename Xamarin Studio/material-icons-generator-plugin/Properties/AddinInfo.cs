e484e11840ffe527dbdf8860cc8c2f3b67078407
interisti <nika.interisti@gmail.com>
2015-03-31 22:06:30 +0400

    add project

---
+++ Xamarin Studio/material-icons-generator-plugin/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"material-icons-generator-plugin", 
+	Namespace = "MaterialIconsGenerator",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("material-icons-generator-plugin")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("material-icons-generator-plugin")]
+[assembly:AddinAuthor ("Nika")]
+
